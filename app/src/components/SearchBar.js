161988c36218c7e5124a6897362e22f53a00d713
annalisapol <annapol2003@gmail.com>
2023-04-18 14:19:19 +0200

    searchbar
    Quasi finita, da sistemare la grafica

--- app/src/components/SearchBar.js
+++ app/src/components/SearchBar.js
@@ -1,23 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 import "./SearchBar.css";
 import Icons from "../components/Icons";
 import { searchIcon } from "../config/IconsPath";
+import { closeIcon } from "../config/IconsPath";
+import { Button } from "@mui/material";
 
 function SearchBar({ placeholder, data }) {
+
+  const [filteredData, setFilteredData] = useState([]);
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    const newFilter = data.filter((value) => {
+      //toLowerCase() serve per rendere la ricerca case insensitive
+      // trasforma tutto in minuscolo
+      return value.title.toLowerCase().includes(searchWord.toLowerCase()); 
+    });
+
+    /* 
+    se searchWord è vuoto allora setta filteredData 
+    a un array vuoto così non viene mostrato nulla
+    ad esempio se scriviamo qualcosa e poi cancelliamo 
+    altrimenti setta filteredData a newFilter 
+    */
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+  };
+
+
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} className="searchInput" />
+        <input type="text" placeholder={placeholder} className="searchInput" onChange={handleFilter} />
         <div className="searchIcon">
-          <Icons
-            iconsColor={"#000"}
+          {
+            filteredData.length === 0           
+            ? 
+            <Button>
+            <Icons iconsColor={"#000"}
             iconsSize={"1.5rem"}
             iconsName={searchIcon}
-          />
+            />
+            </Button>
+            :
+            <Button id="clearButton" onClick={clearInput}>
+            <Icons iconsColor={"#000"}
+            iconsSize={"1.5rem"}
+            iconsName={closeIcon}
+            />
+            </Button>
+          }
+          
         </div>
       </div>
+      {filteredData.length !== 0 && (
       <div className="dataResult">
-        {data.map((value, key) => {
+        {/* slice(0, 15) serve per limitare il numero di risultati a 15 */}
+        {filteredData.slice(0, 15).map((value, key) => {
           return (
             <div>
               {" "}
@@ -26,6 +73,7 @@ function SearchBar({ placeholder, data }) {
           );
         })}
       </div>
+      )}
     </div>
   );
 }
