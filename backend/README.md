a366b61237ae39f60d81bbb594b678d88391efb0
Filippo Terzi <filof330@gmail.com>
2023-04-24 17:55:00 +0200

    Backend + autenticazione + mongoDB

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+Struttura DB:
+2 tipi di account: utente (VIP o meno) e moderatore
+
+Utente normale ha accesso solo alla app.
+Utente VIP ha accesso alla SMM dashboard, il moderatore ha accesso alla moderator dashboard.
+
+Nella sezione "account" l'utente avrá l'opzione "esegui upgrade a VIP", determinare a quali condizioni si può fare.
---
+++ backend/model/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  vip: {
+    type: Boolean,
+    default: false,
+  },
+  refreshToken: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
