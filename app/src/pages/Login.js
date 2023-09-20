0c4251726e5b58849e671e4054c5498bdc2e84de
Albenni <albertobenni@gmail.com>
2023-09-20 12:14:57 +0200

    better, faster, stronger

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -11,6 +11,7 @@ import { Image, Tab, Tabs } from "react-bootstrap";
 import axios from "../api/axios";
 
 import ErrorMessage from "../components/ErrorMessage";
+import theme from "../config/theme";
 
 const LOGIN_URL = "/auth";
 
@@ -53,113 +54,122 @@ function Login() {
 
   return (
     <>
-    
       <TopBar isnotloginpage={false} />
-    <div className="container-fluid" style={{backgroundColor:" rgb(184, 226, 240)", userSelect: "none"}}>
       <div
-        className="container-fluid row"
-        style={{
-          height: "90vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-around",
-        }}
+        className="container-fluid"
+        style={{ backgroundColor: " rgb(184, 226, 240)", userSelect: "none" }}
       >
-        <div className="col-sm-4">
-          <div className="row">
-            <Image src={logo} fluid  />
+        <div
+          className="container-fluid row"
+          style={{
+            height: "90vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <div className="col-sm-4">
+            <div className="row">
+              <Image src={logo} fluid />
+            </div>
           </div>
-        </div>
-        <div className="col-sm-3">
-          <Tabs defaultActiveKey="accedi" className="mb-3" justify>
-            <Tab eventKey="accedi" title="Accedi">
-              <form
-                className="text-align-center container"
-                onSubmit={submitForm}
-              >
-                <div className="form-group py-2">
-                  {/* <h3>Accedi</h3> */}
-                  <ErrorMessage
-                    error="Email o password non valida"
-                    visible={loginFailed}
-                  />
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Inserisci email"
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
+          <div
+            className="col-sm-3"
+            style={{
+              backgroundColor: theme.colors.white,
+              padding: "2rem",
+              borderRadius: "1rem",
+            }}
+          >
+            <Tabs defaultActiveKey="accedi" className="mb-3" justify>
+              <Tab eventKey="accedi" title="Accedi">
+                <form
+                  className="text-align-center container"
+                  onSubmit={submitForm}
+                >
+                  <div className="form-group py-2">
+                    {/* <h3>Accedi</h3> */}
+                    <ErrorMessage
+                      error="Email o password non valida"
+                      visible={loginFailed}
+                    />
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Inserisci email"
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
 
-                <div className="form-group py-2">
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Inserisci password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                <p>
-                  <Link to="NewPassword">Hai dimenticato la password</Link>?
-                </p>
+                  <div className="form-group py-2">
+                    <label>Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Inserisci password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <p>
+                    <Link to="NewPassword">Hai dimenticato la password</Link>?
+                  </p>
 
-                <button type="submit" className="text btn btn-dark">
-                  Accedi
-                </button>
-              </form>
-            </Tab>
-            <Tab eventKey="registrati" title="Registrati">
-              <form
-                className="text-align-center container"
-                // onSubmit={submitForm}
-              >
-                <div className="form-group py-2">
-                  {/* <ErrorMessage
+                  <button type="submit" className="text btn btn-dark">
+                    Accedi
+                  </button>
+                </form>
+              </Tab>
+              <Tab eventKey="registrati" title="Registrati">
+                <form
+                  className="text-align-center container"
+                  // onSubmit={submitForm}
+                >
+                  <div className="form-group py-2">
+                    {/* <ErrorMessage
                     error="Email o password non valida"
                     visible={loginFailed}
                   /> */}
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Inserisci email"
-                    // onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Inserisci email"
+                      // onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
 
-                <div className="form-group py-2">
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Inserisci password"
-                    // onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                <div className="form-group py-2">
-                  <label>Conferma password</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Inserisci password"
-                    // onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                <p>
-                  <Link to="NewPassword">Hai dimenticato la password</Link>?
-                </p>
+                  <div className="form-group py-2">
+                    <label>Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Inserisci password"
+                      // onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group py-2">
+                    <label>Conferma password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Inserisci password"
+                      // onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <p>
+                    <Link to="NewPassword">Hai dimenticato la password</Link>?
+                  </p>
 
-                <button type="submit" className="text btn btn-dark">
-                  Accedi
-                </button>
-              </form>
-            </Tab>
-          </Tabs>
+                  <button type="submit" className="text btn btn-dark">
+                    Accedi
+                  </button>
+                </form>
+              </Tab>
+            </Tabs>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
