ba9be7bbbae5b6cb1176f4f86ecb6101bfaf9703
annalisapol <annapol2003@gmail.com>
2023-10-01 18:27:21 +0200

    fetch login non funzionante

--- SMM/src/app/login/login.component.ts
+++ SMM/src/app/login/login.component.ts
@@ -6,10 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email: string = ''; // Dichiarazione della proprietà username
+
+  username: string = ''; // Dichiarazione della proprietà username
   password: string = ''; // Dichiarazione della proprietà password
 
   logosrc: string = "./assets/SLogo.png"; // Dichiarazione della proprietà logo
-  onSubmit() {
+
+  changeUsername(event: any) {
+    this.username = event.target.value;
+  } // Funzione che cambia il valore della proprietà username
+
+  changePassword(event: any) {  
+    this.password = event.target.value;
   }
+
+  onSubmit() {
+    console.log('Username: ' + this.username + ' Password: ' + this.password);
+    fetch('http://localhost:3500/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        user: this.username,
+        pwd: this.password
+      })
+    }).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+
+
 }
