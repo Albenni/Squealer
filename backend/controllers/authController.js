ab8cc35fdd84d98e33de4a88dc253fc8d6b7d45d
annalisapol <annapol2003@gmail.com>
2023-10-03 23:56:51 +0200

    api smm login
    da controllare

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -104,6 +104,61 @@ const userLogin = async (req, res) => {
   }
 };
 
-const smmLogin = async (req, res) => {};
+const smmLogin = async (req, res) => {
+const { user, pwd } = req.body;
+
+if (!user || !pwd)
+  return res
+    .status(400)
+    .json({ message: "Username and password are required." });
+
+const foundUser = await User.findOne({ username: user }).exec();
+if (!foundUser) return res.status(404).json({ message: "User not found." });
+// confronti le password nel caso in cui sia salvata come hash
+// const match = await bcrypt.compare(pwd, foundUser.password);
+const match = pwd === foundUser.password; // controllo se password coincidono
+
+if (!foundUser.professional) return res.status(404).json({ message: "User not professional." });
+
+// da controllare
+const foundSmm = await Smm.find({ smmId: foundUser.username }).exec();
+
+if (!foundSmm) return res.status(404).json({ message: "No VIP associated." });
+
+if (match) {
+  // create JWTs
+  const accessToken = jwt.sign(
+    {
+      UserInfo: {
+        username: foundUser.username,
+        id: foundUser._id,
+        isMod: false,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "10m" }
+  );
+
+  const refreshToken = jwt.sign(
+    { username: foundUser.username },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "1d" }
+  );
+  // Saving refreshToken with current user
+  foundUser.refreshToken = refreshToken;
+  const result = await foundUser.save();
+
+  // Creates Secure Cookie with refresh token
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  res.json({ accessToken, userid: foundUser._id });
+} else {
+  res.sendStatus(401);
+}};
 
 module.exports = { userLogin, modLogin, smmLogin };
