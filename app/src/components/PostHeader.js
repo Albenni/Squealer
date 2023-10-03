6c7220b13150825902d6e0c0e3d83c15a7de119e
Albenni <albertobenni@gmail.com>
2023-10-03 11:36:07 +0200

    click user

--- app/src/components/PostHeader.js
+++ app/src/components/PostHeader.js
@@ -1,15 +1,23 @@
+import "../css/PostHeader.css";
 import theme from "../config/theme";
 import { Avatar } from "@mui/material";
 import { PatchCheckFill } from "react-bootstrap-icons";
+import { Button } from "react-bootstrap";
 
 function PostHeader({ item }) {
+  function handleUserClick() {
+    console.log("Da fare redirect alla pagina con i post utente");
+  }
+
   return (
-    <div className="row" style={{ pointerEvents: "none" }}>
+    <div className="row">
       <div className="d-flex">
         <div className="m-1">
           <Avatar src={item.propic} />
         </div>
-        <div className="p-1">{item.name}</div>
+        <div className="p-1 usernametext" onClick={handleUserClick}>
+          {item.name}
+        </div>
 
         {item.verified && (
           <div className="p-1">
