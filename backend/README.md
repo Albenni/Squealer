1f950ee13706f5bcf20bb4a7373641ee74cc9601
Filippo Terzi <filof330@gmail.com>
2023-10-04 18:11:50 +0200

    (un)follow keyword

--- backend/README.md
+++ backend/README.md
@@ -7,7 +7,6 @@ api da fare (poche):
 
 - Differenziare tra canali pubblici e canali privati -> un utente fa richiesta per un canale privato, se l'admin lo accetta dopo può leggere e scrivere
 
-- Keyword: canali senza admin in cui tutti hanno tutti i permessi, ci si può iscrivere alle keyword,
 - Modificare il modello Squeal aggiungendo le keyword
 
 - vip manda richiesta e smm accetta per accoppiarsi, upgrade account a pro
--- backend/controllers/followersController.js
+++ backend/controllers/followersController.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Follower = require("../models/Follower");
 const Channel = require("../models/Channel");
+const Keyword = require("../models/Keyword");
 
 const followChannel = async (req, res) => {
   if (!req.authorized) return res.status(403);
@@ -47,7 +48,54 @@ const unfollowChannel = async (req, res) => {
   }
 };
 
+const followKeyword = async (req, res) => {
+  if (!req.authorized) return res.status(403);
+
+  if (!mongoose.Types.ObjectId.isValid(req?.params?.userId))
+    return res.status(400).json({ message: "User ID invalid" });
+  if (!mongoose.Types.ObjectId.isValid(req?.body?.keywordId))
+    return res.status(400).json({ message: "Channel ID not valid" });
+
+  const keyword = await Keyword.findById(req.body.keywordId);
+  if (!keyword) return res.status(400).json({ message: "Keyword not found" });
+
+  try {
+    const result = await Follower.create({
+      followingUserId: req.params.id,
+      followedId: req.body.keywordId,
+      followedType: "Keyword",
+    });
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+};
+
+const unfollowKeyword = async (req, res) => {
+  if (!req.authorized) return res.status(403);
+
+  if (!mongoose.Types.ObjectId.isValid(req?.params?.userId))
+    return res.status(400).json({ message: "User ID invalid" });
+  if (!mongoose.Types.ObjectId.isValid(req?.params?.keywordId))
+    return res.status(400).json({ message: "Keyword ID not valid" });
+
+  try {
+    const result = await Follower.findOneAndDelete({
+      followingUserId: req.params.userId,
+      followedId: req.params.keywordId,
+      followedType: "Keyword",
+    });
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+};
+
 module.exports = {
   followChannel,
   unfollowChannel,
+  followKeyword,
+  unfollowKeyword,
 };
