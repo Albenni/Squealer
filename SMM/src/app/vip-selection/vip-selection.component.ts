04a9cd599721fc076cd3a133a50ab4e5be331995
annalisapol <annapol2003@gmail.com>
2023-10-19 20:59:47 +0200

    mapping degli item
    map

--- SMM/src/app/vip-selection/vip-selection.component.ts
+++ SMM/src/app/vip-selection/vip-selection.component.ts
@@ -1,7 +1,9 @@
 import { Component,  OnInit } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { SharedService } from '../shared.service';
-import { DefaultOptionsInterceptor } from '../default-options.interceptor';
+import  {catchError} from 'rxjs/operators';
+import {throwError} from 'rxjs';
+
 @Component({
   selector: 'app-vip-selection',
   templateUrl: './vip-selection.component.html',
@@ -10,11 +12,12 @@ import { DefaultOptionsInterceptor } from '../default-options.interceptor';
 export class VipSelectionComponent {
   
   selectedAccount:string = "";
-  vipIdList: string[] = [];
+  vipList: string[] = [];
+  usernamesId: string[] = [];
   smmUsername: string = this.sharedService.smmUsername;
   smmId: string = this.sharedService.smmId;
   token: string = this.sharedService.accessToken;
-  accountList: string[] = [];
+
   /*
     per accedere a smm username
     this.sharedService.smmUsername
@@ -30,14 +33,39 @@ export class VipSelectionComponent {
       }),
       withCredentials: true 
     };
+    
 
     // Effettua una richiesta GET all'API per ottenere la lista degli account
-    this.http.get<string[]>('http://localhost:3500/users/'+ this.smmId +'/vips?onlyAccepted=true').subscribe(data => {
-      console.log(data);
-      this.vipIdList = data;
+    this.http.get<string[]>('http://localhost:3500/users/'+ this.smmId +'/vips?onlyAccepted=true').pipe(
+      catchError((error: any) => {
+        // Gestisci l'errore qui
+        console.error('Si è verificato un errore:', error);
+        return throwError('Errore gestito');
+      })
+    )
+    .subscribe(data => {
+      const dataJson = JSON.stringify(data);
+  
+      this.vipList = dataJson.split(',');
+      console.log('Data:' + this.vipList);
       
-    })
+      const vipIdList = this.dataJson.map(item => item._id);
+    });
+
+    
+
+    this.vipList.forEach((id) => {
+      
+    });
   }
+
+  getUserName(id: number): void {
+    this.http.get('http://localhost:3500/users/'+id).subscribe((data: any) => {
+      this.usernamesId[id] = data.username;
+    });
+  
+  }
+
 }
 
 
