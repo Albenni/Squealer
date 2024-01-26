1d9383b2c7ceed436031b1e7bd754e264a607d50
Filippo Terzi <filof330@gmail.com>
2024-01-26 23:17:38 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -32,13 +32,13 @@ const userLogin = async (req, res) => {
         isSmm: false,
       },
     },
-    process.env.ACCESS_TOKEN_SECRET,
+    "9f84bd1cdbf79d572e3e1c97397ce0e48cae6fb55af451c27414e162fc5bdaf0343161f815cbcdafdcb4fbb4bada731620e60dafa49d11fc3fb20b5d959fb569",
     { expiresIn: "10m" }
   );
 
   const refreshToken = jwt.sign(
     { username: foundUser.username, isMod: false, isSmm: false },
-    process.env.REFRESH_TOKEN_SECRET,
+    "bb91eabf9b0115b805169a8f5e837cb285feccbe4be6d1dcafba05ae0f2a0439a4ee5679ae238aaa4098941a5526f8fd0c7bc6b0efd6fb9a3232051fcf34695c",
     { expiresIn: "1d" }
   );
   // Saving refreshToken with current user
@@ -91,13 +91,13 @@ const smmLogin = async (req, res) => {
         isSmm: true,
       },
     },
-    process.env.ACCESS_TOKEN_SECRET,
+    "9f84bd1cdbf79d572e3e1c97397ce0e48cae6fb55af451c27414e162fc5bdaf0343161f815cbcdafdcb4fbb4bada731620e60dafa49d11fc3fb20b5d959fb569",
     { expiresIn: "10m" }
   );
 
   const refreshToken = jwt.sign(
     { username: foundUser.username, isMod: false, isSmm: true },
-    process.env.REFRESH_TOKEN_SECRET,
+    "bb91eabf9b0115b805169a8f5e837cb285feccbe4be6d1dcafba05ae0f2a0439a4ee5679ae238aaa4098941a5526f8fd0c7bc6b0efd6fb9a3232051fcf34695c",
     { expiresIn: "1d" }
   );
   // Saving refreshToken with current user
@@ -120,15 +120,16 @@ const modLogin = async (req, res) => {
 
   if (!user || !pwd)
     return res
-      .status(400)
+      .sendStatus(400)
       .json({ message: "Username and password are required." });
 
   const foundUser = await Moderator.findOne({ username: user }).exec();
-  if (!foundUser) return res.status(401); //Unauthorized
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
   // evaluate password
   // const match = await bcrypt.compare(pwd, foundUser.password);
   const match = pwd === foundUser.password; // controllo se password coincidono
-  if (!match) return res.status(401).json({ message: "Incorrect password" });
+  if (!match)
+    return res.sendStatus(401).json({ message: "Incorrect password" });
 
   // create JWTs
   const accessToken = jwt.sign(
@@ -140,13 +141,13 @@ const modLogin = async (req, res) => {
         isSmm: false,
       },
     },
-    process.env.ACCESS_TOKEN_SECRET,
+    "9f84bd1cdbf79d572e3e1c97397ce0e48cae6fb55af451c27414e162fc5bdaf0343161f815cbcdafdcb4fbb4bada731620e60dafa49d11fc3fb20b5d959fb569",
     { expiresIn: "1d" }
   );
 
   const refreshToken = jwt.sign(
     { username: foundUser.username, isMod: true, isSmm: false },
-    process.env.REFRESH_TOKEN_SECRET,
+    "bb91eabf9b0115b805169a8f5e837cb285feccbe4be6d1dcafba05ae0f2a0439a4ee5679ae238aaa4098941a5526f8fd0c7bc6b0efd6fb9a3232051fcf34695c",
     { expiresIn: "1d" }
   );
   // Saving refreshToken with current user
